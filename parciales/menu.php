7a1814721d536e542f91dad2b54675b6e231f0b4
Miguel <profesor.maac@gmail.com>
2023-01-23 17:40:08 -0300

    primer commit

---
+++ parciales/menu.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edukte</title>
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+</head>
+<body>
+    <header>
+        <div class="cabecera" >
+            <img src="img/Cabecera pagina liceo.jpg" alt="" width="1160px" height="100px">
+        </div>
+    </header>
+    <div class="nav-bg">
+        <nav class="menu-principal contenedor-menu">
+            <a href="index.php">Inicio</a>
+            <a href="estudiantes.php">Matrícula</a>
+            <a href="busqueda.php">Busqueda</a>
+            <a href="#">Reportes</a>
+            <a href="#">Acceso Usuarios</a>
+        </nav>
+    </div>
+    
+</body>
+</html>
