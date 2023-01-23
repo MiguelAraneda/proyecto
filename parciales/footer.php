c558b901d49eb7738d4d1cdb266fc07d275f3689
Miguel <profesor.maac@gmail.com>
2023-01-23 17:58:30 -0300

    agregando imagenes

---
+++ parciales/footer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer class="footer">
+        <section class="pie">
+            <img src="img/edukte 1.png" alt="" width="400px" height="150px">
+        </section>
+        <section class="pie">
+            <p>Todos los Derechos Reservados Edukte</p>
+        </section>
+        <section class="pie">
+            <p>Dirección: Los Corrales 103, El Principal, Pirque</p>
+            <p>Telefono de Contacto +56 9 54210980</p>
+            <p>Email contacto@edukte.cl</p>
+        </section>
+    </footer>
+</body>
+</html>
